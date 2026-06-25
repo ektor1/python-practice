d0f1f801ce1da391d25bab3887ee4ad49166f51f
ektor1 <hc.nlks@gmail.com>
2026-06-25 22:15:49 +0100

    main() goes at the top

--- pascals-triangle.py
+++ pascals-triangle.py
@@ -1,5 +1,17 @@
 from math import factorial
 
+def main():
+    construct_pascals_triangle(5) # triangle formatting is completely off but we love it just the way it is
+
+
+def construct_pascals_triangle(n: int) -> None:
+    """Construct a pascals triangle of n row/s"""
+
+    for i in range(n + 1):
+        for j in range(i + 1):
+            val = get_triangle_cell(i, j)
+            print(val, end = " ")
+        print()
 
 def get_triangle_cell(n: int, k: int) -> int:
     """Calculate value at position (n, k)"""
@@ -12,20 +24,6 @@ def get_triangle_cell(n: int, k: int) -> int:
     formula = factorial(n) / (factorial(k) * factorial(n - k))
     return int(formula)
 
-
-def construct_pascals_triangle(n: int) -> None:
-    """Construct a pascals triangle of n row/s"""
-
-    for i in range(n + 1):
-        for j in range(i + 1):
-            val = get_triangle_cell(i, j)
-            print(val, end = " ")
-        print()
-
-
-def main():
-    construct_pascals_triangle(20) # triangle formatting is completely off but we love it just the way it is
-    
 
 if __name__ == "__main__":
     main()
